6665e21b494a3b6874fc531703a311a6e4159ccc
fantasticAstro <fullstackninja0805@gmail.com>
2022-12-15 12:23:49 -0800

    New Pages

---
+++ client/src/views/contact/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div :class="$style.component">
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'contact-view',
+});
+</script>
+
+<style lang="scss" module>
+.component {
+}
+</style>
---
+++ client/src/views/services/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div :class="$style.component">
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'services-view',
+});
+</script>
+
+<style lang="scss" module>
+.component {
+}
+</style>
